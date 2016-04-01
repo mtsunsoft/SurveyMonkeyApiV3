6621bb0479a522b14d25dd15671c1ec6ff36f658
Christopher Lott <c_lott01@hotmail.com>
2016-04-01 14:48:20 -0700

    add users/me

---
+++ SurveyMonkeyApiV3/Modules/UserService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SurveyMonkeyApiV3.Models;
+using SurveyMonkeyApiV3.Models.Responses;
+using SurveyMonkeyApiV3.Modules.Networking;
+
+namespace SurveyMonkeyApiV3.Modules
+{
+    public class UserService
+    {
+        public static async Task<Me> GetSurveys()
+        {
+            Me response = await SurveyMonkeyRequest.GetRequest<Me>("/users/me");
+            return response;
+        }
+    }
+}
