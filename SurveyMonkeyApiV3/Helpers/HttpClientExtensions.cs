7714bb52a1e11f76697b55c394dd80373a32dc5a
Christopher Lott <c_lott01@hotmail.com>
2016-04-21 12:46:57 -0700

    Patch http method

---
+++ SurveyMonkeyApiV3/Helpers/HttpClientExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyMonkeyApiV3.Helpers
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<HttpResponseMessage> PatchAsync(this HttpClient client, string requestUri,
+            HttpContent iContent)
+        {
+            return await PatchAsync(client, new Uri(requestUri), iContent);
+        }
+
+        public static async Task<HttpResponseMessage> PatchAsync(this HttpClient client, Uri requestUri, HttpContent iContent)
+        {
+            var method = new HttpMethod("PATCH");
+            var request = new HttpRequestMessage(method, requestUri)
+            {
+                Content = iContent
+            };
+
+            HttpResponseMessage response = new HttpResponseMessage();
+            try
+            {
+                response = await client.SendAsync(request);
+            }
+            catch (TaskCanceledException e)
+            {
+                Debug.WriteLine("ERROR: " + e.ToString());
+            }
+
+            return response;
+        }
+    }
+}
