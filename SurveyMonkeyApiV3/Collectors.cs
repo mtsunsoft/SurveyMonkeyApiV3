c89dbfb57cf62708f4265c1b9636a5c067c9ba8a
Manolis Tsagias <mtsagias@sunsoft.gr>
2016-05-12 11:43:15 +0300

    add tests for new methods

--- SurveyMonkeyApiV3/Collectors.cs
+++ SurveyMonkeyApiV3/Collectors.cs
@@ -19,12 +19,6 @@ namespace SurveyMonkeyApiV3
             return response.data;
         }
 
-        public static async Task<List<Recipient>> GetCollectorRecipients(long collectorId)
-        {
-            BaseResponse<Recipient> response = await SurveyMonkeyRequest.GetRequest<BaseResponse<Recipient>>(string.Format("/collectors/{0}/recipients", collectorId));
-            return response.data;
-        }
-
         public static async Task<Recipient> GetCollectorRecipient(long collectorId, long recipientId)
         {
             Recipient response = await SurveyMonkeyRequest.GetRequest<Recipient>(string.Format("/collectors/{0}/recipients/{1}", collectorId, recipientId));
